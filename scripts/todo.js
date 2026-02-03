7b7947bad121479b8417b00635f06a0a77f74f65
Nighty <flander1059@gmail.com>
2026-02-03 12:59:58 +0300

    сhange deletion logic in task

--- scripts/todo.js
+++ scripts/todo.js
@@ -45,7 +45,7 @@ export const initTodo = () => {
       <li class="todo__item todo-item" data-js-todo-task-item>
                       <input class="todo-item__checkbox" type="checkbox" />
                       <span class="todo-item__label" data-js-todo-task-label>${todoValue}</span>
-                      <button class="todo-item__button todo-item__button--edit">
+                      <button class="todo-item__button todo-item__button--edit" data-js-todo-edit-button>
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path
                             d="M21.1739 6.812C21.7026 6.28342 21.9997 5.56647 21.9998 4.81885C21.9999 4.07124 21.703 3.35421 21.1744 2.8255C20.6459 2.29679 19.9289 1.99971 19.1813 1.99962C18.4337 1.99952 17.7166 2.29642 17.1879 2.825L3.84193 16.174C3.60975 16.4055 3.43805 16.6905 3.34193 17.004L2.02093 21.356C1.99509 21.4425 1.99314 21.5343 2.01529 21.6219C2.03743 21.7094 2.08285 21.7892 2.14673 21.853C2.21061 21.9168 2.29055 21.9621 2.37809 21.9841C2.46563 22.0061 2.55749 22.004 2.64393 21.978L6.99693 20.658C7.3101 20.5628 7.59511 20.3921 7.82693 20.161L21.1739 6.812Z"
@@ -95,21 +95,24 @@ export const initTodo = () => {
 
   const removeTask = () => {
     todoListElement.addEventListener('click', (e) => {
-      const currentRemoveButton = e.target.closest('[data-js-todo-remove-button]')
-      if (!currentRemoveButton) return
-      const currentTask = currentRemoveButton.closest('[data-js-todo-task-item]')
-      const currentTaskValue = currentTask.querySelector('[data-js-todo-task-label]').textContent
+      const currentRemoveButtonElement = e.target.closest('[data-js-todo-remove-button]')
+      if (!currentRemoveButtonElement) return
+      const currentTaskElement = currentRemoveButtonElement.closest('[data-js-todo-task-item]')
+      const currentTextTaskLabel = currentTaskElement.querySelector('[data-js-todo-task-label]').textContent
+
+      const idTask = getIdTask(currentTextTaskLabel)
 
       todosArr.forEach((todo, index) => {
-        if (todo === currentTaskValue) {
+        if (index === idTask) {
           todosArr.splice(index, 1)
           setItemLocalStorage(todosArr)
         }
       });
-      currentTask.remove()
+      currentTaskElement.remove()
       toggleClasses()
     })
   }
+
 
   const toggleClasses = () => {
     if (todosArr.length !== 0) {
@@ -119,6 +122,10 @@ export const initTodo = () => {
       todoEmptyElement.classList.remove(stateClasses.isVisible)
       todoFilterElement.classList.add(stateClasses.isVisible)
     }
+  }
+
+  const getIdTask = (TextTaskLabel) => {
+    return todosArr.findIndex((todo) => todo === TextTaskLabel)
   }
 
   removeTask()
